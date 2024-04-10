c2c0dc2344321ddd82bc4f95d1f3afed13e9933d
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-04-10 14:16:37 +0300

    @factorial

--- maths/factorial.js
+++ maths/factorial.js
@@ -2,15 +2,17 @@
 //find the factorial of numbers upto n
 
 //2. WHAT IS A FACTORIAL
-//the factorial on a non negative integer is the product of all positive integers less or equal to n
+//the factorial of a non negative integer is the product of all positive integers less or equal to n
 
 const factorial = (n) => {
   let result = 1;
 
   for (let i = 2; i <= n; i++) {
-    result = result * i;
+    result = result * i; //O(n)
   }
   return result;
 };
+
+//big-O = O(n) => linear
 
 module.exports = factorial;
