b30def86281ff1499279f10f490c1e887cfd0f18
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-04-12 19:24:22 +0300

    @prime-algorithm

--- maths/index.js
+++ maths/index.js
@@ -10,7 +10,7 @@ const app = express();
 //CONSOLE STATEMENTS FOR EACH ALGO
 // console.log(fibonacci(5));
 // console.log(factorial(5));
-console.log(primeNumber(3));
+console.log(primeNumber(5));
 
 app.listen(3005, () => {
   console.log("server running on port 3005");
--- maths/prime_number.js
+++ maths/prime_number.js
@@ -1,8 +1,24 @@
-//Prime Number is a positve number that is only divisible by one or itself
+//1. determnine if 'n' is a prime no
+
+// Note: a prime number is a natural no greater than one that is not product of two smaller natural numbers
 
 const primeNumber = (n) => {
-  const num = n / 2;
-  return !Number.isInteger(num);
+  if (n < 2) {
+    return false;
+  }
+
+  //we use math.sqrt() to optimize the solution
+  //The above is based on reality that the square root of a number being  checked 'n' in our case, is always larger than one of the natural numbers provided for multiplication
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
+
+//big-O=> O(sqrt(n)) => an optimized solution
 
 module.exports = primeNumber;
